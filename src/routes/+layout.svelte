bca0f6c1276032c62d9a98151d405952ecbd768a
Luke Gobin <luke.gobin1@icloud.com>
2025-11-21 19:27:11 -0600

    Create Header Component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,16 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+    import logo from '$lib/assets/logo.png';
+    import headerBackground from '$lib/assets/financial-frontier.png';
+    
 	import '$lib/assets/style.css'
 
-	import logo from '$lib/assets/logo.png';
-	import discord from "$lib/assets/discord.png";
-	import instagram from "$lib/assets/instagram.png";
-	import linkedin from "$lib/assets/linkedin.png";
-	import uh from "$lib/assets/uh.png";
+    import Header from '$lib/Header.svelte';
 
 	let { children } = $props();
 </script>
 
-<header>
-    <!-- Make a "Skip to Main Content" button for accessibility/People who tab-->
-    <img src={ logo } alt="Logo"/>
-
-    <div class="desktop-only social-media-banner">
-        <h2>Connect With Us:</h2>
-        <div class="logos">
-            <a href="https://discord.gg/ZBwh4YgxRA" target="_blank"><img src={ discord } alt="Discord" /></a>
-            <a href="https://www.instagram.com/cooginteractive/" target="_blank"><img src={ instagram } alt="Instagram" /></a>
-            <a href="https://www.linkedin.com/company/coog-interactive" target="_blank"><img src={ linkedin } alt="Linkedin" /></a>
-            <a href="https://uh.campuslabs.com/engage/organization/cooginteractive" target="_blank"><img src={ uh } alt="UH Get Involved" /></a> <!-- Placeholder -->
-        </div>
-    </div>
-</header>
+<Header background={headerBackground}/>
 
 <nav>
     <a class="mobile-only" id="menu-button" onmousedown={ toggleNavbar }>≡ Menu</a> <!-- FIXME -->
