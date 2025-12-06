b356c1e62e5737ccadf6f1cbba831890ca046841
Luke Gobin <luke.gobin1@icloud.com>
2025-12-06 13:47:17 -0600

    Begin Working on Studio Page

---
+++ src/routes/studio/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import pic from './assets/mhgu.jpg';
+</script>
+
+<div class="row">
+    <div class="picture-collumn">
+        <img src={ pic } alt="Mirror's Edge by EA DICE" class="left-image"/>
+    </div>
+    <section class="info-section">
+        <h1>OUR STUDIO</h1>
+        <p>
+            Founded by a passionate mix of industry professionals, academics, and student developers, Coog Interactive's studio provides students with something most programs seldomly offer: a true to life, studio-style production experience.
+        </p>
+        <p>
+            You won’t just hastily develop a game for a class assignment or hobby project—you’ll work in a structured pipeline, collaborate across disciplines with agile teams of like-minded individuals, and be provided the support to ship a playable title of professional quality by the end of each semester.
+        </p>
+        <a href="https://discord.gg/ZBwh4YgxRA" target="_blank">
+            <button>JOIN THE DISCORD</button>
+        </a>
+    </section>
+</div>
