9487224c2a93b581fda6cefecc059442e0478366
Luke Gobin <luke.gobin1@icloud.com>
2025-12-11 11:40:27 -0600

    Add More Featured Games

--- src/routes/games/data.ts
+++ src/routes/games/data.ts
@@ -12,7 +12,38 @@ export const games = [
             "Arturo Murga",
             "Shaheer Ismail",
         ],
-        desc: "2D top-down action-adventure game with hack-and-slash combat and retro 16-bit pixel art built in Unity for PC.",
+        desc: "Tower of Judgement is a 2D top-down action-adventure game with hack-and-slash combat and retro 16-bit pixel art built in Unity for PC.",
         href: "https://goldengoosegames.itch.io/tower-of-judgement",
     },
+    {
+        header: "https://i.ytimg.com/vi_webp/7TMIgWTM3do/maxresdefault.webp",
+        name: "The Tides of Andunië",
+        authors: [
+            "Timothy (Hayden) Groh",
+            "Gustavo Buenrostro",
+            "Alexis Orellana",
+            "Trevor Topolski",
+            "Mohamed Abdelrahman",
+            "Tasneem Harianawala",
+            "Darryl Sims",
+            "Jason Vu",
+        ],
+        desc: "The Tides of Andunië is a top-down action RPG. Drop into a Tolkien-inspired world to defeat the pirates and save the Island of Erressëa.",
+        href: "https://nospoonstudio.itch.io/the-tides-of-andunie"
+    },
+    {
+        header: "https://img.itch.zone/aW1nLzI0NDU2MjQ4LnBuZw==/347x500/%2Fw0xoq.png",
+        name: "Wild Pete",
+        authors: [
+            "Joseph Giap (me!)",
+            "Emma Nguyen",
+            "Cody Luc",
+            "Johann Perez",
+            "Thu Pham",
+            "Angel Savaria",
+            "Yousif Saieb",
+        ],
+        desc: "A pixel-art side scrolling combat game, where you must escape and fight through the prison to escape!",
+        href: "https://josephgiap123.itch.io/wild-pete"
+    }
 ]
